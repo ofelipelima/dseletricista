f8a60baac49dbd4dcee6432027631cbc20edb8ca
Felipe Lima <eu@felipelima.com.br>
2024-01-24 15:21:34 -0300

    Create politica-de-privacidade.php

---
+++ politica-de-privacidade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui"/>
+
+    <!-- FONTS -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="css/stroke-gap.min.css" rel="stylesheet" type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Poppins:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" rel="stylesheet">
+    <link href="css/idangerous.swiper.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css?v=2" rel="stylesheet" type="text/css" />
+
+    <link rel="shortcut icon" href="img/favicon-e.ico" />
+    <title>Política de Privacidade - DS Instalações Elétricas</title>
+
+    <meta name="keywords" content="política de privacidade, lgpd, DS instalações elétricas" />
+    <meta name="description" content="Política de Privacidade do site e tratamento de informações - LGPD">
+</head>
+<body>
+
+    <?php include "./header.html" ?>
+
+    <div id="content-wrapper">
+
+        <!-- TT-PAGELABEL -->
+        <div class="tt-pagelabel background-block" style="background-image:url(img/breadcrumbs/breadcrumbs.jpg);">
+            <div class="container">
+                <h1 class="tt-pagelabel-title">Política de Privacidade</h1>
+            </div>
+        </div>
+
+        <!-- TT-BREADCRUMB -->
+        <div class="tt-breadcrumbs-wrapper">
+            <div class="container">
+                <ul class="tt-breadcrumbs">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>Política de Privacidade</span></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="bg-1">
+            <div class="container">
+                <div class="empty-space marg-lg-b85 marg-md-b50 marg-xs-b30"></div>
+                <div class="tt-tab-wrapper">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="simple-text">
+                                        <h2><span style="color: rgb(68, 68, 68);">Política Privacidade</span></h2><p><span style="color: rgb(68, 68, 68);">A sua privacidade é importante para nós. É política do DS Instalações Elétricas respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="https://dseletricista.com.br/">DS Instalações Elétricas</a>, e outros sites que possuímos e operamos.</span></p><p><span style="color: rgb(68, 68, 68);">Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</span></p><p><span style="color: rgb(68, 68, 68);">Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</span></p><p><span style="color: rgb(68, 68, 68);">Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</span></p><p><span style="color: rgb(68, 68, 68);">O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas&nbsp;</span><a href="https://politicaprivacidade.com/" rel="noopener noreferrer" target="_blank" style="background-color: transparent; color: rgb(68, 68, 68);">políticas de privacidade</a><span style="color: rgb(68, 68, 68);">.</span></p><p><span style="color: rgb(68, 68, 68);">Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</span></p><p><span style="color: rgb(68, 68, 68);">O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</span></p><p><span style="color: rgb(68, 68, 68);"><ul><li><span style="color: rgb(68, 68, 68);">O serviço Google AdSense que usamos para veicular publicidade usa um cookie DoubleClick para veicular anúncios mais relevantes em toda a Web e limitar o número de vezes que um determinado anúncio é exibido para você.</span></li><li><span style="color: rgb(68, 68, 68);">Para mais informações sobre o Google AdSense, consulte as FAQs oficiais sobre privacidade do Google AdSense.</span></li><li><span style="color: rgb(68, 68, 68);">Utilizamos anúncios para compensar os custos de funcionamento deste site e fornecer financiamento para futuros desenvolvimentos. Os cookies de publicidade comportamental usados ​​por este site foram projetados para garantir que você forneça os anúncios mais relevantes sempre que possível, rastreando anonimamente seus interesses e apresentando coisas semelhantes que possam ser do seu interesse.</span></li><li><span style="color: rgb(68, 68, 68);">Vários parceiros anunciam em nosso nome e os cookies de rastreamento de afiliados simplesmente nos permitem ver se nossos clientes acessaram o site através de um dos sites de nossos parceiros, para que possamos creditá-los adequadamente e, quando aplicável, permitir que nossos parceiros afiliados ofereçam qualquer promoção que pode fornecê-lo para fazer uma compra.</span></li></ul><p><br></p></span></p><h3><span style="color: rgb(68, 68, 68);">Compromisso do Usuário</span></h3><p><span style="color: rgb(68, 68, 68);">O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o DS Instalações Elétricas oferece no site e com caráter enunciativo, mas não limitativo:</span></p><ul><li><span style="color: rgb(68, 68, 68);">A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</span></li><li><span style="color: rgb(68, 68, 68);">B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, </span><span style="color: rgb(33, 37, 41);"><a href='https://bbebbet.br.com' style='color:inherit !important; text-decoration: none !important; font-size: inherit !important;'>bbebbet</a></span><span style="color: rgb(68, 68, 68);"> ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</span></li><li><span style="color: rgb(68, 68, 68);">C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do DS Instalações Elétricas, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</span></li></ul><h3><span style="color: rgb(68, 68, 68);"><br>Mais informações</span></h3><p><span style="color: rgb(68, 68, 68);">Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.</span></p><p><span style="color: rgb(68, 68, 68);">Esta política é efetiva a partir de&nbsp;24 de Janeiro de 2024.</span></p>
+                                        <p>
+                                            <div class="text-center">
+                                                <a class="c-btn type-1 size-2 font-2 color-3" href="https://wa.me/5548998414008?text=Olá! Vim pelo site e gostaria de mais informações sobre montagem de padrão de energia" target="_blank" rel="nofollow noopener noreferrer"><span><i class="fa fa-whatsapp" aria-hidden="true"></i> Chame o eletricista agora pelo Whatsapp</span></a>
+                                            </div>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="empty-space marg-sm-b30"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="empty-space marg-lg-b50 marg-md-b50 marg-xs-30"></div>
+
+                <div class="bg-2">
+                    <div class="tt-two-blocks">
+                        <div class="container">
+                            <div class="empty-space marg-lg-b80 marg-md-b60"></div>
+                            <div class="tt-two-blocks-img background-block" style="background-image:url(img/two-blocks/two-blocks.jpg);"></div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h3 class="tt-block-title type-2 color-2 c-h3"><i class="fa fa-calendar" aria-hidden="true"></i><span>Entrar em contato</span></h3>
+                                    <div class="empty-space marg-lg-b45 marg-md-b30"></div>
+                                    <form action="envia-contato.php" method="post" class="tt-shedule-form" >
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input class="c-input" type="text" placeholder="Nome" name="nome" required>
+                                            </div>                              
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <input class="c-input" type="email" placeholder="Email" name="email" required>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <input class="c-input" type="text" placeholder="Telefone" name="telefone">
+                                            </div>                                    
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <textarea maxlength="5000" data-msg-required="Insira uma mensagem." rows="4" class="form-control" placeholder="Mensagem" name="msg" id="message" required></textarea>
+                                            </div>                                   
+                                        </div>
+                                        <div class="empty-space marg-lg-b10 marg-md-b10 marg-xs-b10"></div>
+                                        <div class="c-btn type-1 size-3 font-3 color-4">
+                                            <input type="submit" value="Enviar">
+                                            <input type="hidden" name="envia" value="ok">
+                                            <input type="hidden" name="pagina" value="Página padrão de energia">
+                                        </div>
+                                        <span class="tt-request-success"></span>                     
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="empty-space marg-lg-b80 marg-md-b60"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="empty-space marg-lg-b50 marg-md-b50 marg-xs-30"></div>
+
+                <!-- TT-BANNER  -->
+                <div class="tt-banner">
+                    <img class="tt-banner-img" src="img/banner/banner.png" height="134" width="144" alt="">
+                    <div class="tt-banner-info">
+                        <h4 class="tt-banner-title">24/7 Emergência ligue: <a href="tel:+5548998414008">(48) 99841-4008</a></h4>
+                        <div class="simple-text size-3 color-3">
+                            <p>Instalação e Manutenção | Projeto e Execução | Padrões de energia</p>
+                        </div>
+                    </div>
+                    <div class="tt-banner-link">
+                        <a class="c-btn type-1 size-4 color-5 no-radius" href="https://wa.me/5548998414008?text=Olá! Vim pelo site e gostaria de mais informações" target="_blank" rel="nofollow noopener noreferrer"><span><i class="fa fa-whatsapp" aria-hidden="true"></i> Whatsapp</span></a>
+                    </div>
+                </div>
+                <!--FECHA TT-BANNER  -->
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <?php include "./footer.html" ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="js/idangerous.swiper.min.js"></script>
+    <script src="js/global.js"></script>
+    
+</body>
+
+</html>
