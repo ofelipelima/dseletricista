352db2521ed8bc0085f0ae9c0f26ea6d3ad35762
Felipe Lima <eu@felipelima.com.br>
2024-01-24 15:33:51 -0300

    lgpd consent

--- iluminacao-de-piscina.php
+++ iluminacao-de-piscina.php
@@ -19,7 +19,7 @@
     <!-- CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" rel="stylesheet">
     <link href="css/idangerous.swiper.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css?v=2" rel="stylesheet" type="text/css" />
+    <link href="css/style.css?v=3" rel="stylesheet" type="text/css" />
 
     <link rel="shortcut icon" href="img/favicon-e.ico" />
     <title>Iluminação de piscina - DS Instalações Elétricas</title>
